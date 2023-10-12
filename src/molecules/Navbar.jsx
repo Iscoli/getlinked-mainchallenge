e6267dde2f74dc297e60ba134cb90ca328249723
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-12 01:54:41 -0700

    modal fixing

--- src/molecules/Navbar.jsx
+++ src/molecules/Navbar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styles from "./Navbar.module.scss";
 import MySVGIcon from "../assets/hambuger.svg";
+import closeIcon from '../assets/Close.svg'
+
+
 function NavBar() {
+  const [modal, setModal] = useState(false);
   const [isLargeScreen, setIsLargeScreen] = useState(
     window.matchMedia("(min-width: 700px)").matches
   );
@@ -26,6 +30,11 @@ function NavBar() {
     };
   }, []);
 
+  const Modal = ()=>{
+    console.log('hello world')
+    setModal(!modal)
+  }
+   console.log(modal)
   return (
     <>
       <nav className={styles.navbarContainer}>
@@ -38,15 +47,46 @@ function NavBar() {
           className={styles.navLinks}
           style={{ display: isLargeScreen ? "flex" : "none" }}
         >
+       
           <p>TimeLine</p>
           <p>Overveiw</p>
           <p>FAQs</p>
           <p>Contact</p>
         </div>
         <div>
-          {isLargeScreen ? <button>Register</button> : <img src={MySVGIcon} />}
+          {isLargeScreen ? <button>Register</button> 
+          : 
+          <div
+           onClick={Modal}
+          >
+            <img
+           style={{cursor:'pionter'}}
+          src={MySVGIcon} />
+          </div>}
         </div>
       </nav>
+
+      <div
+      styles={{position:'relative'}}
+      >
+     {modal && (
+    <div className={styles.modal}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '30px' }}>
+        <p>getLinked</p>
+        <img 
+        onClick={Modal}
+        src={closeIcon} />
+      </div>
+      <p>Timeline</p>
+      <p>Overview</p>
+      <p>FAQs</p>
+      <p>Contact</p>
+      <button className={styles.btn}>
+        Register
+      </button>
+    </div>
+  )}
+      </div>
       <div className={styles.Line1}></div>
     </>
   );
